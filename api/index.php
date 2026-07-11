c7f1af6c46e703f19a6a997f71660c1e6e65d909
MSyafiq19 <mhdsyafiqsyahmi19@gmail.com>
2026-07-11 21:25:35 +0700

    pasang alat pendeteksi error di layar

--- api/index.php
+++ api/index.php
@@ -1,3 +1,11 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-require __DIR__ . '/../public/index.php';
+try {
+    require __DIR__ . '/../public/index.php';
+} catch (\Throwable $e) {
+    echo "<h1>Error Detected di Vercel</h1>";
+    echo "<pre style='white-space: pre-wrap;'>" . (string) $e . "</pre>";
+}
